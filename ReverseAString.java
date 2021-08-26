4e468d879d1a04be6325a97809fac2e86b1d93b7
himanshu-brown <himanshu.brown@gmail.com>
2021-08-26 21:06:30 +0530

    Reverse a String

---
+++ ReverseAString.java
@@ -0,0 +1,16 @@
+class ReverseAString
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        // Reverse the string str
+        String s = "";
+        
+        for(int i=str.length()-1; i>=0; i--)
+            s += str.charAt(i);
+    
+        return s;
+        
+    }
+}
