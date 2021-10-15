d57d0beeb3e7f368bbd49884bbf88f30270c87f2
Himanshu Sharma <himanshu.brown@gmail.com>
2021-10-15 16:53:16 +0530

    Permutation of a given string

---
+++ PermutationOfAGivenString.java
@@ -0,0 +1,26 @@
+class PermutationOfAGivenString{
+    
+    public void permutation(String rem,List<String> list, String str) {
+       if(rem == null) return;
+       
+       if(rem.length() == 0) {
+           list.add(str);
+       }
+       
+       for(int i=0; i<rem.length(); i++) {
+           String newC = str + rem.charAt(i);
+           String newR = rem.substring(0,i)+rem.substring(i+1);
+           permutation(newR,list, newC);
+       }
+   }
+    
+    public List<String> find_permutation(String S) {
+        
+        String ans="";
+       List<String> list = new ArrayList<>();
+       permutation(S,list,ans);
+       Collections.sort(list);
+       return list;
+        
+    }
+}
